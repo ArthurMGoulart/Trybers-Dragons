979d368aa38de75a055c8535cad37de09f685f1d
ArthurMGoulart <budiarthur@gmail.com>
2022-03-10 16:43:13 -0300

    fix: createdRacesInstances uses this to get and Class. to add on constructor

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -3,10 +3,16 @@ import MaxLifePoints from './Enums';
 
 class Dwarf extends Race {
   private _maxLifePoints: number;
+  private static _createdRacesInstances = 0;
 
   constructor(n: string, d: number) {
     super(n, d);
     this._maxLifePoints = MaxLifePoints.DWARF;
+    Dwarf._createdRacesInstances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return this._createdRacesInstances;
   }
 
   get maxLifePoints(): number {
--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -3,10 +3,16 @@ import MaxLifePoints from './Enums';
 
 class Orc extends Race {
   private _maxLifePoints: number;
+  private static _createdRacesInstances = 0;
 
   constructor(n: string, d: number) {
     super(n, d);
     this._maxLifePoints = MaxLifePoints.ORC;
+    Orc._createdRacesInstances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return this._createdRacesInstances;
   }
 
   get maxLifePoints(): number {
