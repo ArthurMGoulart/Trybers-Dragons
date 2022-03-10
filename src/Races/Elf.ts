4cbc64ab010c0dc55ccb29813f06a3de4edb569a
ArthurMGoulart <budiarthur@gmail.com>
2022-03-10 14:59:05 -0300

    feat: enum MaxLifePoints, feat: class Elf

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -1,12 +1,11 @@
 import Race from '.';
-import MaxLifePoints from './Enums';
 
 class Dwarf extends Race {
   private _maxLifePoints: number;
 
   constructor(n: string, d: number) {
     super(n, d);
-    this._maxLifePoints = MaxLifePoints.DWARF;
+    this._maxLifePoints = 99;
   }
 
   get maxLifePoints(): number {
---
+++ src/Races/Enums.ts
@@ -0,0 +1,8 @@
+enum MaxLifePoints {
+  DWARF = 80,
+  ELF = 99,
+  HALFLING = 60,
+  ORC = 74,
+}
+
+export default MaxLifePoints;
