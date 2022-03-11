457986ab5286a2578008e0f07a261ea1034eb850
ArthurMGoulart <budiarthur@gmail.com>
2022-03-11 14:42:47 -0300

    fix: Dragon now can have diff strenghts but fixed lifePoints

--- src/Dragon.ts
+++ src/Dragon.ts
@@ -1,8 +1,9 @@
 import Monster from './Monster';
 
 class Dragon extends Monster {
-  constructor(l = 999, s = 99) {
-    super(l, s);
+  constructor(s = 63) {
+    super(undefined, s);
+    this._lifePoints = 999;
   }
 }
 
